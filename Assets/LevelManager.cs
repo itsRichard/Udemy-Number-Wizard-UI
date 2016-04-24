d653edd41efd0fb4de5af782191cebcb6fc73d1e
itsRichard <rtl@uw.edu>
2016-04-23 22:50:05 -0700

    Added level/scene switching buttons.
    Functionality to levelload()

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 	
-	public void LoadLevel(string name){
-		Debug.Log ("Level load requested for: " + name);
-		}
+	public void LoadLevel (string name){
+		Debug.Log ("Load Level Request: " + name);
+		Application.LoadLevel (name);
+	}
 	
 	public void QuitRequest(){
 		Debug.Log ("Quit Requested");
+		Application.Quit();
 	}
 
 }
